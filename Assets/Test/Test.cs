3733626cc600b333c1b6ecbc7ed01fb22d61d6a5
yow1 <chanyow@163.com>
2017-05-14 23:05:53 +0800

    up

--- Assets/Test/Test.cs
+++ Assets/Test/Test.cs
@@ -2,8 +2,17 @@
 using System.Collections;
 using System.Collections.Generic; 
 using UnityEngine.UI; 
+using UnityEditor;
+using System.IO;
 
+ 
 public class Test : MonoBehaviour
 {
-	
+	void Start()
+	{
+		
+		Debug.Log("obj: " + Selection.activeObject); 
+		Debug.Log("active path: " + AssetDatabase.GetAssetPath(Selection.activeObject)); 
+		Debug.Log("active filename: " + Path.GetDirectoryName(AssetDatabase.GetAssetPath(Selection.activeObject))); 
+	}
 }
